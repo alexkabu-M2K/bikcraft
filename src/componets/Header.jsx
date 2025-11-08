40090cb707d865b12a88f7f53b1081416c4cb0d9
Alexkabu <alexkabu@outlook.com>
2025-11-08 08:42:01 -0300

    ajustes codigo

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -1,7 +1,7 @@
 
 const Header = ({ children }) => {
 
-    const style = "bg-black flex justify-between items-center pt-[0] pr-25 pb-[0] pl-25 text-white text-2xl font-mono h-[3.5rem]";
+    /* const style = "bg-black flex justify-between items-center pt-[0] pr-25 pb-[0] pl-25 text-white text-2xl font-mono h-[3.5rem]";*/
 
     return <header className={style}>{children}</header>
 };
